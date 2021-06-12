2bc54bf8950388d4785a719daec2aac3570b25d1
Mason Stooksbury <mason.e.stooksbury@saic.com>
2021-06-11 22:34:00 -0500

    Added titlescreen and music

---
+++ TitleScreen/TitleScreen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var scene_path_to_load
+
+func _ready() -> void:
+	# Connect the pressed() method of every scene to this scene
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "_on_menu_button_pressed", [button.next_scene])
+		
+func _on_menu_button_pressed(next_scene):
+	# Do this check so that we eliminate the possibility of clicking
+	#		randomly and possibly ending up on the wrong scene
+	if scene_path_to_load == null or scene_path_to_load == "":
+		scene_path_to_load = next_scene
+		
+	get_tree().change_scene(scene_path_to_load)
