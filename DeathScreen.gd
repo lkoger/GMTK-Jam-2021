ffb93fd489b3bf9c5a5e514896e23b91c17d2088
Mason Stooksbury <mason.e.stooksbury@saic.com>
2021-06-13 11:21:47 -0500

    Finished death screen

--- DeathScreen.gd
+++ DeathScreen.gd
@@ -5,22 +5,7 @@ export(String, FILE, "*.tscn") var main_menu
 
 func _ready() -> void:
 	get_node('/root/Globals')._play('death')
-	
-#	var minutes = Globals.time_alive_score / 60
-#	var seconds = Globals.time_alive_score % 60
 	var final_time_text = ""
-#	if(minutes == 1):
-#		final_time_text = str(minutes) + " minute "
-#	elif(minutes == 0):
-#		pass
-#	else:
-#		final_time_text = str(minutes) + " minutes "
-#
-#	if(seconds == 1):
-#		final_time_text += str(seconds) + " second "
-#	elif(seconds == 0):
-#		pass
-#	else:
 	final_time_text += str(Globals.time_alive_score) + " seconds"
 	
 	$MarginContainer/CenterContainer/HBoxContainer/TotalStats/MiddleSection/Stats/Time.text = final_time_text
@@ -30,15 +15,11 @@ func _ready() -> void:
 	else:
 		$MarginContainer/CenterContainer/HBoxContainer/TotalStats/MiddleSection/Stats/Rounds.text = str(Globals.rounds_alive_score) + " rounds"
 
-	#var ambi_energy_collected = 0
-	#var dextr_energy_collected = 0
-	#var ambi_hits_taken = 0
-	#var dextr_hits_taken = 0
 	
-	print(Globals.ambi_energy_collected)
-	print(Globals.dextr_energy_collected)
-	print(Globals.ambi_hits_taken)
-	print(Globals.dextr_hits_taken)
+	$MarginContainer/CenterContainer/HBoxContainer/AMB1sStats/Stats/Numbers/Energy.text = str(Globals.ambi_energy_collected)
+	$MarginContainer/CenterContainer/HBoxContainer/AMB1sStats/Stats/Numbers/Damage.text = str(Globals.ambi_hits_taken)
+	$MarginContainer/CenterContainer/HBoxContainer/D3XTRsStats/Stats/Numbers/Energy.text = str(Globals.dextr_energy_collected)
+	$MarginContainer/CenterContainer/HBoxContainer/D3XTRsStats/Stats/Numbers/Damage.text = str(Globals.dextr_hits_taken)
 
 
 func _on_NewGame_pressed() -> void:
